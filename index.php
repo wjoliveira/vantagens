8869b444d594e1ebb3ec7a6f34e962838996ae31
Wellington de Jesus Oliveira <wellington.dejesusoliveira@gmail.com>
2019-02-17 15:14:12 -0300

    Autoload Wjoliveira classes

--- index.php
+++ index.php
@@ -1,66 +1,21 @@
-<!DOCTYPE html>
-<html lang="pt-BR">
-    <head>
-      <meta charset="utf-8">   
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      <title>Clube de Vantagens</title>
-  
-      <!-- Bootstrap links CDN-->
-			<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+<?php
 
+require_once("vendor/autoload.php");
 
-    <style>
-      .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-      }
+$app = new \Slim\Slim();
 
-      @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-          font-size: 3.5rem;
-        }
-      }
-    </style>
-    <!-- Custom styles for this template -->
-    <link href="css/estilo-customizado.css" rel="stylesheet">
+$app->config('debug', true);
 
-			
-    </head>
-    <body>
-      
-     <!-- Dependência jQuery-->
-     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
-     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.bundle.min.js" integrity="sha384-zDnhMsjVZfS3hiP7oCBRmfjkQC4fzxVxFhBx8Hkz2aZX8gEvA/jsP3eXRCvzTofP" crossorigin="anonymous"></script>
-		
-			<form class="form-signin">
-				<div class="text-center mb-4">
-    			<img class="mb-4" src="imagem/logo-habitat.png" alt="" width="216" height="72">
-    			<h1 class="h3 mb-3 font-weight-normal">Clube de Vantagens</h1>
-  			</div>
+$app->get('/', function() {
 
-				<div class="form-label-group">
-    			<input type="email" id="inputEmail" class="form-control" placeholder="Email" required autofocus>
-    			<label for="inputEmail">Email</label>
-  			</div>
+	$sql = new Wjoliveira\DB\Sql();
 
-  			<div class="form-label-group">
-    			<input type="password" id="inputPassword" class="form-control" placeholder="Senha" required>
-    			<label for="inputPassword">Senha</label>
-  			</div>
-				
-			  <div class="checkbox mb-3">
-			    <label>
-		      	<a href="#">Esqueceu sua senha?</a>
-			    </label>
-			  </div>
-				<button class="btn btn-lg btn-primary btn-block" type="submit">Entrar</button>
+    $results = $sql->select("SELECT * FROM tb_users");
 
-				<div class="text-center checkbox mb-3">
-					<label>
-						Ainda não tem uma conta? <a href="#"><strong>Cadastre-se</strong></a>
-					</label>
-				</div>
-			  <p class="mt-5 mb-3 text-muted text-center">&copy; 2017-2019</p>
-			</form>			
-    </body>
- </html>
+    echo json_encode($results);
+
+});
+
+$app->run();
+
+ ?>
